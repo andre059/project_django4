7bb80faa44a18e54d391a093d093e247e4eedcf9
andre059 <andreyperm159@mail.ru>
2023-07-27 21:05:14 +0500

    3.3

--- catalog/models.py
+++ catalog/models.py
@@ -4,11 +4,24 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        # ordering = ('name',)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.CharField(max_length=150, verbose_name='описание')
     image = models.ImageField(upload_to='preview/', verbose_name='изображение ', **NULLABLE)
-    category = models.CharField(max_length=150, verbose_name='категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     purchase_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='цена за килограмм', **NULLABLE)
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     last_modified_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
@@ -26,14 +39,3 @@ class Product(models.Model):
         ordering = ('category', 'name',)
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.CharField(max_length=150, verbose_name='описание')
-
-    def __str__(self):
-        return f'{self.name} {self.description}'
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ('name',)
