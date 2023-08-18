6162c65dd589a4a69a516f3c5b506d2a3153e550
andre059 <andreyperm159@mail.ru>
2023-08-18 17:16:27 +0500

    11

--- catalog/models.py
+++ catalog/models.py
@@ -38,3 +38,16 @@ class Product(models.Model):
         ordering = ('category', 'name',)
 
 
+class Subject(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+
+    def __str__(self):
+        return f'{self.title} ({self.product})'
+
+    class Meta:
+        verbose_name = 'предмет'
+        verbose_name_plural = 'предметы'
+
