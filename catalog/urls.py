41c615c555e9441b846c92ed39fd9793e73dc525
andre059 <andreyperm159@mail.ru>
2023-08-22 12:44:17 +0500

    12

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('<int:pk>/views/', ProductDetailView.as_view(), name='inc_products_detail'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
     path('<int:pk>/delete/', ProductDeleteView.as_view(), name='delete'),
-    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='update'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update'),
 ]
