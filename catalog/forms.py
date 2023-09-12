4f2186b88caac592955ebe9a8c6c9a47bbbd9e6a
andre059 <andreyperm159@mail.ru>
2023-09-12 16:42:14 +0500

    17.1

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,8 +4,8 @@ from catalog.models import Product, Subject
 
 
 class FormStyleMixin:
-    def __init__(self, **args):
-        super().__init__(**args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
