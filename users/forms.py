991c9a3a4f0757dc92c0bc7aaee1924573e67fe7
andre059 <andreyperm159@mail.ru>
2023-10-04 14:25:48 +0500

    21

--- users/forms.py
+++ users/forms.py
@@ -5,14 +5,12 @@ from users.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
 class UserProfileForm(UserChangeForm):
-
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar')
@@ -21,3 +19,11 @@ class UserProfileForm(UserChangeForm):
         super().__init__(*args, **kwargs)
 
         self.fields['password'].widget = forms.HiddenInput()
+
+
+# class SignupForm(UserCreationForm):
+    # email = forms.EmailField(max_length=200, help_text='Required')
+
+    # class Meta:
+        # model = User
+        # fields = ('username', 'email', 'password1', 'password2')
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from users import views
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import ProfileView, RegisterView
 
 app_name = UsersConfig.name
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    # path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.activate,
+         # name='activate'),
 ]
