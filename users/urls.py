543b9dff070aa069c13e5933cf9e743c2c106998
andre059 <andreyperm159@mail.ru>
2023-10-12 22:57:41 +0500

    24 (регистрация по почте работает)

--- users/urls.py
+++ users/urls.py
@@ -12,8 +12,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('success_verify/', views.success_verify, name='success_verify'),
-    path('invalid_link/', views.invalid_link, name='invalid_link'),
-    # path('signup/', views.signup, name='signup'),
     path('activate/<uidb64>[0-9A-Za-z]+<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}', views.activate, name='activate')
 ]
--- users/views.py
+++ users/views.py
@@ -32,30 +32,6 @@ class RegisterView(CreateView):
         super().__init__()
         self.method = None
 
-        # def form_valid(self, form):
-        # Создаем пользователя, но пока не сохраняем
-        # user = form.save(commit=False)
-        # user.is_active = False  # Устанавливаем активность пользователя в False
-        # user.save()  # Сохраняем пользователя
-
-        # Генерируем и сохраняем токен подтверждения почты
-        # token = get_random_string(length=32)
-        # email_token = EmailVerificationToken.objects.create(user=user, token=token)
-
-        # Отправляем письмо с токеном подтверждения
-        # subject = 'Подтверждение почты'
-        # message = f'Привет {user.username}, перейдите по ссылке для подтверждения вашей почты: ' \
-        # f'{self.request.scheme}://{self.request.get_host()}/users/activate/{token}/'
-        # from_email = settings.DEFAULT_FROM_EMAIL
-        # recipient_list = [user.email]
-        # send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-
-        # Сохраняем токен
-        # email_token.save()
-
-        # Перенаправляем пользователя на другую страницу после регистрации
-        # return super().form_valid(form)
-
     def form_valid(self, form):
 
         # Получаем пользователя из формы и делаем его не активным
@@ -81,64 +57,24 @@ class RegisterView(CreateView):
         return super().form_valid(form)
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            # сохранить форму в памяти, а не в базе данных
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-
-            # Сгенерировать и сохранить токен подтверждения электронной почты
-            token = account_activation_token.make_token(user)
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
-            email_token = EmailVerificationToken.objects.create(user=user, token=token, uid=uid)
-
-            # Отправить электронное письмо с подтверждением
-            current_site = get_current_site(request)
-            mail_subject = 'Ссылка для активации была отправлена на ваш электронный адрес'
-            message = render_to_string('acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uidb64': uid,
-                'token': token,
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
-
-            return HttpResponse('Пожалуйста, подтвердите свой адрес электронной почты, чтобы завершить регистрацию')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'signup.html', {'form': form})
-
-
 def activate(request, uidb64, token):
     User = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid, )
+        user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True  # устанавливаем поле is_active в значение True
         user.save()  # сохраняем изменения в базе данных
-        return redirect(reverse_lazy('success_verify'))
-        # return HttpResponse('Благодарим вас за подтверждение по электронной почте. '
-        # 'Теперь вы можете войти в свою учетную запись.')
+        # return render(request, 'success_verify.html')
+        return HttpResponse('Благодарим вас за подтверждение по электронной почте. '
+                            'Теперь вы можете войти в свою учетную запись.')
     else:
-        return redirect(reverse_lazy('invalid_link'))
-        # return HttpResponse('Ссылка для активации недействительна!')
-
-
-def success_verify(request):
-    return render(request, 'success_verify.html')
-
-
-def invalid_link(request):
-    return render(request, 'invalid_link.html')
+        # return render(request, 'invalid_link.html')
+        # return redirect(reverse_lazy('invalid_link'))
+        return HttpResponse('Ссылка для активации недействительна!')
 
 
 class ProfileView(UpdateView):
