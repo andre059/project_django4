bf6f14b8cc7ef2bf7ad131373858caeaa7dbd560
andre059 <andreyperm159@mail.ru>
2023-10-21 00:57:28 +0500

    26.2 ()

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update'),
     path('open_images/', views.open_images, name='open_images'),
     path('version/', views.product_list_activ, name='version'),
-    path('home/', home, name='home'),
+    # path('home/', home, name='home'),
 ]
