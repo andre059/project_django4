d92ff2cb17929a7f1c0a81bc85d6e124341aa88c
andre059 <andreyperm159@mail.ru>
2023-10-25 22:23:28 +0500

    27.1 (фото)

--- catalog/templatetags/mediapath.py
+++ catalog/templatetags/mediapath.py
@@ -1,18 +1,33 @@
 from django import template
 from django.conf import settings
+from django.db.models.fields.files import FieldFile
 from django.forms import Media
 
 register = template.Library()
 
 
-@register.simple_tag
-def mediapath(image_path):
-    return f"{settings.MEDIA_URL}{image_path}"
+# @register.simple_tag
+# def mediapath(image_path):
+    # return f"{settings.MEDIA_URL}{image_path}"
 
 
 # @register.simple_tag
 # def path_tag(format_string):
     # return settings.MEDIA_URL + str(format_string)
+
+
+@register.simple_tag
+@register.filter()
+def mediapath(data: FieldFile) -> str:
+    """
+    Make url path to media
+
+    Examples:
+        <img src="{{ object.image|mediapath }}" />
+        or
+        <img src="{% mediapath object.image %}" />
+    """
+    return data.url if data else '#'
 
 
 @register.filter
